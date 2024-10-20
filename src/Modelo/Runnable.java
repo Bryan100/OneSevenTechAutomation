47558d302ffd767bf2592e58c965cb84d7cdc800
BryanIMPDev <bryan.perez@impartner.com>
2024-10-20 09:49:32 -0400

    Mi 2do Commit :D

--- src/Modelo/Runnable.java
+++ src/Modelo/Runnable.java
@@ -5,7 +5,7 @@ public class Runnable {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Hello World!");
+		System.out.print("Hello World! Testeando mi 2do Commit y Push :D");
 		
 	}
 	
